7a72677293c33f528e149f6434b957dd8f13c65a
Milena Quina <milena.xavier@iv2.com.br>
2020-11-12 13:36:02 -0300

    Alterações npm

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,43 +1,59 @@
-import React from 'react';
-import styles from './estilo.module.css';
-import img from '../../img/logo.svg';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import styles from "./estilo.module.css";
+import img from "../../img/logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
 
-class Home extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            Pesquisa: ""
-        }
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Pesquisa: "",
     };
-    ChamarPag(e){
-        const { Pesquisa } = this.state;
-        if(Pesquisa !== ""){ // Condição para não entar vazio na busca;
-            e.preventDefault();
-            this.props.history.push(`/search/${Pesquisa}`)
-        }else{
-            e.preventDefault();
-            this.props.history.push(`/`)
-        }
-    };
-    render(){
-        return(
-            <div className={styles.CampoTotal}>
-                <div className="CampoConteudo">
-                    <img src={img} alt="" name="logosw" className={styles.LogoSw}/>
-                    <form>
-                        <div className="ConteudoTexto">
-                            <input name="campo-de-pesquisa" className={styles.campoDePesquisa} type="text" size="50" placeholder="Buscar..." autoComplete="off"
-                               autoFocus value={this.state.Pesquisa} onChange={e => this.setState({ Pesquisa: e.target.value })}></input>
-                        </div>
-                        <div className={styles.DoBotao}>
-                            <button id="botao"  className="btn btn-warning" onClick={($event) => this.ChamarPag($event)}>Buscar</button>
-                        </div>
-                    </form>
-                </div>
+  }
+  ChamarPag(e) {
+    const { Pesquisa } = this.state;
+    if (Pesquisa !== "") {
+      // Condição para não entar vazio na busca;
+      e.preventDefault();
+      this.props.history.push(`/search/${Pesquisa}`);
+    } else {
+      e.preventDefault();
+      this.props.history.push(`/`);
+    }
+  }
+  render() {
+    return (
+      <div className={styles.CampoTotal}>
+        <div className="CampoConteudo">
+          <img src={img} alt="" name="logosw" className={styles.LogoSw} />
+          <form>
+            <div className="ConteudoTexto">
+              <input
+                name="campo-de-pesquisa"
+                className={styles.campoDePesquisa}
+                type="text"
+                size="50"
+                placeholder="Buscar..."
+                autoComplete="off"
+                autoFocus
+                value={this.state.Pesquisa}
+                onChange={(e) => this.setState({ Pesquisa: e.target.value })}
+              ></input>
             </div>
-        )
-    };
+            <div className={styles.DoBotao}>
+              <button
+                id="botao"
+                className="btn btn-warning"
+                onClick={($event) => this.ChamarPag($event)}
+              >
+                Buscar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
@@ -46,4 +62,3 @@ export default Home;
 // autocomplete Para não aparecer o historico da pesquisa;
 // onchange para mudar o estado de Pesquisa;
 // size é o comprimento do input, altura no css;
-
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import React, {Component,Suspense} from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import Home from './components/Home/home';
-import Busca from './components/Buscar/busca';
-import Person from './components/Person/person';
+import React, { Component, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/home";
+import Busca from "./components/Buscar/busca";
+import Person from "./components/Person/person";
 
 class Routes extends Component {
   render() {
@@ -10,9 +10,15 @@ class Routes extends Component {
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
-            <Route exact path="/" component={props => <Home {...props}/>}/>
-            <Route path="/search/:Pesquisa" component={props => <Busca {...props}/>}/>
-            <Route path="/person/:id/:Pesquisa" component={props => <Person {...props}/>}/>
+            <Route exact path="/" component={(props) => <Home {...props} />} />
+            <Route
+              path="/search/:Pesquisa"
+              component={(props) => <Busca {...props} />}
+            />
+            <Route
+              path="/person/:id/:Pesquisa"
+              component={(props) => <Person {...props} />}
+            />
           </Switch>
         </Suspense>
       </BrowserRouter>
